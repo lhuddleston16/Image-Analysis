02f8d1df7efed9fdb011e5f436221b216e30d3f6
Levi Huddleston <levilhuddleston@gmail.com>
2022-06-30 17:30:32 -0700

    update config

--- setup.py
+++ setup.py
@@ -2,24 +2,26 @@ from setuptools import setup
 import sys
 
 
-def forbid_publish():
-    argv = sys.argv
-    blacklist = ["register", "upload"]
+# def forbid_publish():
+#     argv = sys.argv
+#     blacklist = ["register", "upload"]
 
-    for command in blacklist:
-        if command in argv:
-            values = {"command": command}
-            print('Command "%(command)s" has been blacklisted, exiting...' % values)
-            sys.exit(2)
+#     for command in blacklist:
+#         if command in argv:
+#             values = {"command": command}
+#             print('Command "%(command)s" has been blacklisted, exiting...' % values)
+#             sys.exit(2)
 
 
-forbid_publish()
+# forbid_publish()
 
 setup(
     name="pachama_test",
-    version="0.0.1",
+    version="0.1",
     description="A package to extract values from raster files.",
-    author="Levi Huddleston",
-    packages=["pachama_test"],
     url="https://github.com/lhuddleston16/pachama_test",
-)
+    author="Levi Huddleston",
+    author_email='levilhuddleston@gmail.com',
+    license='MIT',
+    packages=["pachama_test"],
+    zip_safe = False)
